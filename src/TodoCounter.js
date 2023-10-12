5feec459166c31e0198a29427c1bd14d2aaf7860
nicknamed <nicknamed19@gmail.com>
2023-10-12 14:24:57 -0500

    Actualización del TodoCounter

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,43 +2,18 @@ import './TodoCounter.css';
 
 function TodoCounter({ completed, total }) {
 
-    const noCero = ( total > 0 && completed > 0);
-    const equal = (completed === total);
-    const noTask = (completed === 0 && total === 0);
-
-    if (noTask) {
-        return(
-            <h1>
-                Agrega una tarea
-            </h1>
-        )
-    } else if (noCero && equal) {
-        return(
-            <h1>
-                completaste todas las tareas
-            </h1>
-        )
-    } else {
-        <h1>
-            {`Haz completado ${completed}  tareas de ${total}.`}
-        </h1>
-    }
-    /*return(
+    return(
         <h1 className="title">
             {
-                noTask
+                (total === 0)
                 ? `Agrega una nueva tarea` 
-                : ''
+                : ((completed === total) 
+                    ? `Completaste todas las tareas.` 
+                    : `Haz completado ${completed}  tareas de ${total}.`
+                  )
             }
-
-            {
-                equal 
-                ? `Completaste todas las tareas.` 
-                : `Haz completado ${completed}  tareas de ${total}.`
-            } 
-
         </h1>
-    )*/
+    )
 }
 
 export { TodoCounter }
