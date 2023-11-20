47ff1c6e3dfa8a8acd3bd45da472708c6e0f08e9
nicknamed <nicknamed19@gmail.com>
2023-11-20 12:47:21 -0500

    Se implementa el contexto y se agrega funcionalidad para abrir y cerrar el modal

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -1,57 +1,59 @@
+import React from 'react';
+import { ErrorTodos } from './ErrorTodos';
+import { LoadingTodos } from './LoadingTodos';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
-import { LoadingTodos } from './LoadingTodos';
-import { ErrorTodos } from './ErrorTodos';
 import { CreateTodoButton } from './CreateTodoButton';
+import { Modal } from './Modal';
+import { TodoContext } from '../context/TodoContext';
 
-function AppUI ({
-    completedTodos,
-    totalTodos,
-    searchValue,
-    setSearchValue,
-    sercheadTodo,
-    checkedTask,
-    deleteTask,
-    loading,
-    error,
-}) {
-    return (
-      <>
-        {loading 
-          ? <LoadingTodos/> 
-          : <>
-              <TodoCounter 
-                completed={completedTodos} 
-                total={totalTodos} 
-              />
+function AppUI () {
 
-              <TodoSearch 
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-              />
+  const {
+    loading, 
+    error,           
+    sercheadTodo, 
+    checkedTask, 
+    deleteTask, 
+    openModal,
+  } = React.useContext(TodoContext)
 
-              <CreateTodoButton />
-            </>
-        }
-        
-        {error && <ErrorTodos/>}
-          
-        <TodoList>
-            {sercheadTodo.map(todo => 
-              <TodoItem 
-              key={todo.text}
-              text={todo.text}
-              completed={todo.completed}         
-              checkedTask={() => checkedTask(todo.text)}
-              deleteTask={() => deleteTask(todo.text)}
-              />
-            )}
-        </TodoList>        
-        
-      </>
-    );
-}
+  return (
+    <>
+      {error && <ErrorTodos/>}
+
+      {loading 
+        ? <LoadingTodos/> 
+        : <>
+            <TodoCounter />
+      
+            <TodoSearch />
+
+            <TodoList>
+              {sercheadTodo.map(todo => 
+                <TodoItem 
+                  key={todo.text}
+                  text={todo.text}
+                  completed={todo.completed}         
+                  checkedTask={() => checkedTask(todo.text)}
+                  deleteTask={() => deleteTask(todo.text)}
+                />
+              )}
+           </TodoList>
+      
+           <CreateTodoButton />
+
+           {openModal &&
+            <Modal>
+            Renderizar TODO
+            </Modal>
+           } 
+            
+          </>}
+    </>
+  )
+}        
 
 export { AppUI };
