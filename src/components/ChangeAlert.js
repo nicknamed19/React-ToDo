ac8c1933a35c0c181a57a1dd78f1338598c13c14
nicknamed <nicknamed19@gmail.com>
2024-02-16 14:09:16 -0500

    Se mejora el componente ChangeAlert y se crea una hoja de estilos

--- src/components/ChangeAlert.js
+++ src/components/ChangeAlert.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { withStorageListener } from './withStorageListener'
+import { Modal } from "./Modal";
+import '../styles/ChangeAlert.css'
 
 function ChangeAlert ({show, toggleShow, synchronizeTodos}) {
     if (show) {
         return (
-            <div>
-                <p>Hubo cambios</p>
-                <button
-                    onClick={() => {
-                        synchronizeTodos()
-                        toggleShow()
-                    }}
-                    
-                >Actualizar info</button>
-            </div>
+            <Modal>
+                <div className="synchronize-container">
+                    <h1 className="content">Parece que hiciste cambios en alguna pestaña o ventana del navegador</h1>
+                    <h2 className="content">¿Quieres sincronizar tus TODO's?</h2>
+
+                    <div className="synchronize-button-container">
+                        <button
+                            className="synchronize-button accept"
+                            onClick={() => {
+                                synchronizeTodos()
+                                toggleShow()
+                            }}
+                            
+                        >Yes!</button>
+                        <button
+                            className="synchronize-button cancel"
+                            onClick={() => {
+                                toggleShow()
+                            }}
+                            
+                        >No</button>
+                    </div>
+                </div>
+            </Modal>
         )
     } else {
         return null
