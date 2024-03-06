f2ce28c09bf207ccceed38b458b020c838d47f11
nicknamed <nicknamed19@gmail.com>
2024-03-06 11:17:17 -0500

    Se añade un icono para la edicion de todos y su respectivo estilo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,15 +11,31 @@ function TodoItem(props){
               `material-symbols-outlined check-hover ${props.completed ? "check-activate" : ''}`
             }
             onClick={props.checkedTask}
-          >check_circle</span>
+          >
+            check_circle
+          </span>
+
           <p className={props.completed ? "task-complete" : ''}>{props.text}</p>
 
         </div>
         
-        <span 
-          className='material-symbols-outlined cancel-hover'
-          onClick={props.deleteTask}
-        >cancel</span>
+        <div className='cancel-edit-container'>
+
+          <span 
+            className="material-symbols-outlined edit-hover"
+            onClick={() => console.log('editar todo')}
+          >
+            edit
+          </span>
+
+          <span 
+            className='material-symbols-outlined cancel-hover'
+            onClick={props.deleteTask}
+          >
+            cancel
+          </span>
+
+        </div>
       </li>
     );
   }
