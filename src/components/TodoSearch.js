4af1152f7d464486ffd3d42fc90e78c7c2e266e4
nicknamed <nicknamed19@gmail.com>
2024-03-07 11:35:06 -0500

    Se implementa nueva funcionalidad para la barra de busqueda y la propiedad de busqueda de la URL

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import '../styles/TodoSearch.css';
+import { useSearchParams } from 'react-router-dom';
 
 function TodoSearch({ searchValue, setSearchValue }) {
-
+    const [searchParams, setSearchParams] = useSearchParams()
+    
     function changeInput(event){
-        setSearchValue(event.target.value)        
+        const value = event.target.value
+        setSearchValue(value)
+        setSearchParams({ search: value})
     };
+    
+    
+    React.useEffect(() => {
+        const search = searchParams.get('search') ?? '';
+        setSearchValue(search);
+    }, [searchParams])
 
     return(
         <div className='containSearch'>
--- src/routes/NewTodoPage.js
+++ src/routes/NewTodoPage.js
@@ -3,6 +3,7 @@ import { useTodos } from "../hooks/useTodos"
 
 function NewTodoPage() {
     const { addTodo, } = useTodos()
+
     return (
         <Form 
             label={'Agrega una nueva tarea'}
